1c2aa518d572206d21c5fb29a577f1f3a759d372
neomadaxx <mfourt@epsi.fr>
2012-12-27 10:08:16 +0100

    class escalier

--- src/elements_decor/Escalier.h
+++ src/elements_decor/Escalier.h
@@ -1,16 +1,19 @@
 #ifndef ESCALIER_H_INCLUDED
 #define ESCALIER_H_INCLUDED
 #include "../elements/element_decor.h"
+#include "../utils//point.h"
 
 using namespace std;
 
 class Escalier : public ElementDecor {
 	private:
-		int hauteur;
+		Point* extremite;
 	public:
 		Escalier();
-		Escalier(double,double,double, int);
+		Escalier(double,double,double,double,double,double);
 		void draw();
+		void drawHorizontalStairs();
+		void drawVerticalStairs();
 		~Escalier();
 };		
 
